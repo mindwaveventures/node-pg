9255c97449031df06eb9dba97d842111c317eedf
lakshmiramalingam-mediwave <lakshmi_ramalingam@mindwaveventures.com>
2023-11-22 18:47:55 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const pgClient = require('./pg-config');
-
+const pgClient = require("./pg-config");
 
 // create application/json parser
 const jsonParser = bodyParser.json();
@@ -15,48 +14,63 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(jsonParser);
 app.use(urlencodedParser);
 
-app.post('/save-user', async function (req, res) {
-    const queryText = 'INSERT INTO users(name) VALUES($1) RETURNING userid,name';
-    const pgRes = await pgClient.query(queryText, [req.body.name]);
-    
-    const postQueryText = 'INSERT INTO posts(postcontent,userid) VALUES($1,$2) RETURNING postid';
-    const postPgRes = await pgClient.query(postQueryText, [req.body.postcontent, pgRes.rows[0].userid]);
+app.post("/save-user", async function (req, res) {
+  const queryText = "INSERT INTO users(name) VALUES($1) RETURNING userid,name";
+  const pgRes = await pgClient.query(queryText, [req.body.name]);
 
-    res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount,
-        postInsert: postPgRes.rows
-    });
+  const postQueryText =
+    "INSERT INTO posts(postcontent,userid) VALUES($1,$2) RETURNING postid";
+  const postPgRes = await pgClient.query(postQueryText, [
+    req.body.postcontent,
+    pgRes.rows[0].userid,
+  ]);
+
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+    postInsert: postPgRes.rows,
+  });
 });
 
-app.patch('/update-user', async function (req, res) {
-    const queryText = 'UPDATE users set name=$1 where userid=$2 RETURNING userid,name';
-    const pgRes = await pgClient.query(queryText, [req.body.name, req.body.userid]);
+app.patch("/update-user", async function (req, res) {
+  const queryText =
+    "UPDATE users set name=$1 where userid=$2 RETURNING userid,name";
+  const pgRes = await pgClient.query(queryText, [
+    req.body.name,
+    req.body.userid,
+  ]);
 
-    res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount
-    });
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+  });
 });
 
-app.get('/', async function (req, res) {
-    const pgRes = await pgClient.query('SELECT name from users LIMIT $1', [req.query.limit || 1]);
+app.get("/item", async function (req, res) {
+  const pgRes = await pgClient.query("SELECT * from items LIMIT $1", [
+    req.query.limit || 1,
+  ]);
 
-    res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount,
-    });
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+  });
 });
 
-app.delete('/remove', async function (req, res) {
-    const pgRes = await pgClient.query('DELETE from users where userid=$1 RETURNING userid', [req.query.userid]);
+app.delete("/remove", async function (req, res) {
+  const pgRes = await pgClient.query(
+    "DELETE from users where userid=$1 RETURNING userid",
+    [req.query.userid]
+  );
 
-    res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount,
-    });
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+  });
 });
 
 app.listen(process.env.PORT, process.env.HOST, () => {
-    console.log(`Server running at http://${process.env.HOST}:${process.env.PORT}/`);
+  console.log(
+    `Server running at http://${process.env.HOST}:${process.env.PORT}/`
+  );
 });
