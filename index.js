da2fe5679a1103de4b04cba2a4e74031777bd1dd
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-11-22 14:36:28 +0530

    get-items, add-items added

--- index.js
+++ index.js
@@ -1,62 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-require('dotenv').config();
-
-const pgClient = require('./pg-config');
-
-
-// create application/json parser
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const pgClient = require("./pg-config");
 const jsonParser = bodyParser.json();
-
-// create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(jsonParser);
 app.use(urlencodedParser);
 
-app.post('/save-user', async function (req, res) {
-    const queryText = 'INSERT INTO users(name) VALUES($1) RETURNING userid,name';
-    const pgRes = await pgClient.query(queryText, [req.body.name]);
-    
-    const postQueryText = 'INSERT INTO posts(postcontent,userid) VALUES($1,$2) RETURNING postid';
-    const postPgRes = await pgClient.query(postQueryText, [req.body.postcontent, pgRes.rows[0].userid]);
+app.get("/", async function (req, res) {
+  const pgRes = await pgClient.query("SELECT item_name from items LIMIT $1", [
+    req.query.limit || 1,
+  ]);
 
-    res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount,
-        postInsert: postPgRes.rows
-    });
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+  });
 });
 
-app.patch('/update-user', async function (req, res) {
-    const queryText = 'UPDATE users set name=$1 where userid=$2 RETURNING userid,name';
-    const pgRes = await pgClient.query(queryText, [req.body.name, req.body.userid]);
+app.post("/add-items", async function (req, res) {
+  const queryText =
+    "INSERT INTO items(item_name,item_content,price,status_of_item) VALUES($1,$2,$3,$4) RETURNING item_id,item_name";
+  const pgRes = await pgClient.query(queryText, [
+    req.body.item_name,
+    req.body.item_content,
+    req.body.price,
+    req.body.status_of_item,
+  ]);
 
-    res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount
-    });
-});
-
-app.get('/', async function (req, res) {
-    const pgRes = await pgClient.query('SELECT name from users LIMIT $1', [req.query.limit || 1]);
-
-    res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount,
-    });
-});
-
-app.delete('/remove', async function (req, res) {
-    const pgRes = await pgClient.query('DELETE from users where userid=$1 RETURNING userid', [req.query.userid]);
-
-    res.json({
-        rows: pgRes.rows,
-        count: pgRes.rowCount,
-    });
+  res.json({
+    rows: pgRes.rows,
+    count: pgRes.rowCount,
+  });
 });
 
 app.listen(process.env.PORT, process.env.HOST, () => {
-    console.log(`Server running at http://${process.env.HOST}:${process.env.PORT}/`);
+  console.log(
+    `Server running at http://${process.env.HOST}:${process.env.PORT}/`
+  );
 });
