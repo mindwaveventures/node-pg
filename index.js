da106bf02e88ea3ed1d921c205ea864ceb06f7a7
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-11-23 16:31:36 +0530

    not found added

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(jsonParser);
 app.use(urlencodedParser);
 const itemRouter = require("./routes/items.route");
 const { errorHandler } = require("./middlewares/errorhandler.middleware");
+const { notfound } = require("./middlewares/notfound.middleware");
 
 // items routes
 app.use("/", itemRouter);
 app.use(errorHandler);
+app.use(notfound);
 
 app.listen(process.env.PORT, process.env.HOST, () => {
   console.log(
