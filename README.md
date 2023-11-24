a4ae546454b97f04959cf5d700bb689ffb0329be
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-11-24 12:41:41 +0530

    final

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # node-pg
+
+# postman-collection:
+
+- Fav-part
+
+http://localhost:5000/fav?user_id=1 --> to display the favorate done by user
+
+based on id
+
+http://localhost:5000/fav?user_id=1&search=al --> to search based on the typed
+
+letter
+
+http://localhost:5000/fav?user_id=2&priceRange=0-25 --> used to filter rating by
+
+the user
+
+http://localhost:5000/fav?user_id=1&search=a&priceRange=0-250 --> to filter id
+
+price and search
+
+http://localhost:5000/fav?user_id=1&sortOrder=desc --> to sort using price by
+
+using desc and asc
+
+- Rating -part:
+
+http://localhost:5000/rating --> to display overall rating for all items
+
+- display-canceled-one:
+
+http://localhost:5000/cancel?user_id=4 --> to display cancel by using user_id
+
+http://localhost:5000/cancel?user_id=4&priceRange=1-15 --> to display cancelled using price range
+
+http://localhost:5000/cancel?user_id=4&priceRange=1-15&search=cook --> search in canceled by using
+
+name of iteams
+
+http://localhost:5000/cancel?user_id=4&sortOrder=asc --> this is used to sort by
+
+using price range
+
+- update order into cancel:
+
+http://localhost:5000/update --> to change value
+
+raw:
+{
+"status": "Cancelled",
+"purchaseid": "10"
+}
--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,5 +1,6 @@
 const pgClient = require("../pg-config");
 
+// update to cancel
 const updateStatusController = async function (req, res) {
   const queryText =
     "UPDATE purcharse SET status=$1 WHERE purchase_id=$2 RETURNING purchase_id, status";
@@ -14,41 +15,31 @@ const updateStatusController = async function (req, res) {
   });
 };
 
+// fav
 const getFavController = async (req, res) => {
   try {
-    let query;
+    if (!req.query.user_id) {
+      return res.status(400).json({ error: "Please provide a user_id" });
+    }
 
-    //http://localhost:5001/fav?user_id=2
+    let query =
+      "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
 
     if (req.query.user_id) {
-      query =
-        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
       query += ` WHERE favourites.user_id = ${req.query.user_id}`;
     }
 
-    //http://localhost:5000/fav?user_id=1&search=al
-
     if (req.query.search) {
-      query =
-        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
       query += ` AND items.item_name ILIKE '%${req.query.search}%'`;
     }
 
-    // Adding sorting
-
     if (req.query.sortOrder) {
-      query =
-        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
-      const sortOrder = req.query.sortOrder === "desc" ? "DESC" : "ASC";
-      query += ` ORDER BY ${sortOrder}`;
+      const sortOrder =
+        req.query.sortOrder.toUpperCase() === "DESC" ? "DESC" : "ASC";
+      query += ` ORDER BY items.price ${sortOrder}`;
     }
 
-    // Adding sorting by price range
-    //http://localhost:5001/fav?priceRange=0-500
-
     if (req.query.priceRange) {
-      query =
-        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
       const priceRanges = req.query.priceRange.split("-");
       const minPrice = parseFloat(priceRanges[0]);
       const maxPrice = parseFloat(priceRanges[1]);
@@ -79,16 +70,36 @@ const overallRatingController = async (req, res) => {
   });
 };
 
+//cancel
 const cancelListController = async (req, res) => {
   try {
-    let query = "SELECT * FROM purcharse WHERE status = 'Cancelled'";
+    if (!req.query.user_id) {
+      return res.status(400).json({ error: "Please provide a user_id" });
+    }
+
+    let query =
+      "SELECT purcharse.*, items.item_name FROM purcharse JOIN items ON purcharse.item_id = items.item_id WHERE purcharse.status = 'Cancelled'";
+
+    if (req.query.user_id) {
+      query += ` AND purcharse.user_id = ${req.query.user_id}`;
+    }
+
+    if (req.query.search) {
+      query += ` AND items.item_name ILIKE '%${req.query.search}%'`;
+    }
+
+    if (req.query.sortOrder) {
+      const sortOrder =
+        req.query.sortOrder.toUpperCase() === "DESC" ? "DESC" : "ASC";
+      query += ` ORDER BY items.price ${sortOrder}`;
+    }
 
     if (req.query.priceRange) {
       const priceRanges = req.query.priceRange.split("-");
       const minPrice = parseFloat(priceRanges[0]);
       const maxPrice = parseFloat(priceRanges[1]);
 
-      query += " AND purcharse.price BETWEEN $1 AND $2";
+      query += ` AND purcharse.price BETWEEN $1 AND $2`;
 
       const pgRes = await pgClient.query(query, [minPrice, maxPrice]);
       res.json({
@@ -107,6 +118,7 @@ const cancelListController = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
 module.exports = {
   updateStatusController,
   getFavController,
