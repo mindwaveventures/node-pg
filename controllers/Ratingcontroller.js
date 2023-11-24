f0434763542613d2d0bc242241e07113d72e2ee9
irshathbanu-mediwave <irshathbanu@mindwaveventures.com>
2023-11-24 14:38:00 +0530

    add the date fixed

---
+++ controllers/Ratingcontroller.js
@@ -0,0 +1,20 @@
+const pgClient = require("../pg-config");
+async function addRatingController(req, res) {
+  try {
+    const queryText =
+      "INSERT INTO rating(item_id,user_id,ratingvalue) VALUES($1,$2,$3) RETURNING item_id,user_id,ratingvalue";
+    console.log(req.body.item_id, req.body.user_id, req.xop.ratingValue);
+    const pgRes = await pgClient.query(queryText, [
+      req.body.item_id,
+      req.body.user_id,
+      req.xop.ratingValue,
+    ]);
+    res.json({
+      rows: pgRes.rows,
+      count: pgRes.rowCount,
+    });
+  } catch (err) {
+    console.log("Unknown Error:", err);
+    res.status(500).json({ error: "Unknown Error" });
+  }
+}
