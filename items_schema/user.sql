3be270434addc4c4c812ab7854258db22ac88d4a
kabipriya-mediwave <kabipriya@mindwaveventures.com>
2023-11-24 18:56:05 +0530

    updated users

--- items_schema/user.sql
+++ items_schema/user.sql
@@ -8,3 +8,7 @@ create table users (
 	phone_no VARCHAR,
 	created_at TIMESTAMP default current_timestamp
 ) ;
+
+ insert into users (first_name, last_name, user_name, email, user_password, phone_no)
+values
+('Kalus', 'Mickalson', 'kalus_Mick19', 'kalusMick@gmail.com', 'bcdbVWGF38', '+1 98481 65848')
