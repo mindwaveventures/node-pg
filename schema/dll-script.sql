ac474d6d07ed1b94c148c4566845db530e54db8b
sineka-mediwave <sineka@mindwaveventures.com>
2023-11-27 14:53:52 +0530

    ddl-cscript

--- schema/dll-script.sql
+++ schema/dll-script.sql
@@ -15,7 +15,7 @@ CREATE TABLE items (
     item_id SERIAL PRIMARY KEY,
     item_name VARCHAR not null,
     item_content VARCHAR,
-    price integer DECIMAL(10, 2) not null,
+    price DECIMAL(10, 2) not null,
     item_count integer not null
 );
 
@@ -28,22 +28,22 @@ CREATE TABLE favourites (
 create table ratings(
     rating_id serial not null primary key, 
     item_id integer references items(item_id) on delete cascade on update cascade,
-    user_id integer references users(user_id) on delete cascade on update cascade,
+    user_id integer references account_users(id) on delete cascade on update cascade,
     rating INT CHECK (rating BETWEEN 1 AND 5)
 );
 
 create table carts(
     cart_id serial not null primary key,
     item_id integer references items(item_id) on delete cascade on update cascade,
-    user_id integer references users(user_id) on delete cascade on update cascade,
+    user_id integer references account_users(id) on delete cascade on update cascade,
     item_name varchar(255) not NULL,
-    price integer DECIMAL(10, 2) not null,
+    price DECIMAL(10, 2) not null,
 );
 
 create table purchases(
     purchase_id serial not null primary key,
     item_id integer references items(item_id) on delete cascade on update cascade,
-    user_id integer references users(user_id) on delete cascade on update cascade,
+    user_id integer references account_users(id) on delete cascade on update cascade,
     date_of_order date default cast(now() as date),
     order_status varchar(255)
     item_price DECIMAL(10, 2) NOT null,
