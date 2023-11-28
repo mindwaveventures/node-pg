e473e09de30f9aa697aa45fe9d32153c673f59dc
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-11-28 18:43:08 +0530

    Users

---
+++ models/items.js
@@ -0,0 +1,38 @@
+// const { DataTypes } = require("sequelize");
+
+module.exports = function model(sequelize, types) {
+  const Items = sequelize.define(
+    "items",
+    {
+      item_id: {
+        type: types.UUID,
+        defaultValue: types.UUIDV4,
+        primarykey: true,
+        unique: true,
+      },
+      item_name: {
+        type: types.STRING,
+        allowNull: false,
+      },
+    },
+
+    {
+      tableName: "items",
+    }
+  );
+
+  //   Users.associate = function (models) {
+  //     Users.hasMany(models.posts, {
+  //       as: "",
+  //       foreignKey: "userId",
+  //       sourceKey: "user_id",
+  //     });
+  //   };
+
+  return Items;
+};
+// item_id SERIAL PRIMARY KEY,
+//     item_name VARCHAR not null,
+//     item_content VARCHAR,
+//     price DECIMAL(10, 2) not null,
+//     item_count integer not null
--- models/users.js
+++ models/users.js
@@ -1,35 +1,75 @@
+// const { DataTypes } = require("sequelize");
+
 module.exports = function model(sequelize, types) {
-    const Users = sequelize.define('users', {
-        uuid: {
-            type: types.UUID,
-            defaultValue: types.UUIDV4,
-            primarykey: true,
-            unique: true,
-        },
-        name: {
-            type: types.STRING,
-            defaultValue: ''
-        },
-        status: {
-            type: types.STRING,
-            defaultValue: 'Active'
-        },
-    }, {
-        tableName: 'users',
-        // defaultScope: {
-        //     where: {
-        //         status: 'Active'
-        //     }
-        // }
-    });
+  const Users = sequelize.define(
+    "users",
+    {
+      //         uuid: {
+      //             type: types.UUID,
+      //             defaultValue: types.UUIDV4,
+      //             primarykey: true,
+      //             unique: true,
+      //         },
+      //         name: {
+      //             type: types.STRING,
+      //             defaultValue: ''
+      //         },
+      //         status: {
+      //             type: types.STRING,
+      //             defaultValue: 'Active'
+      //         },
+      //     }, {
+      //         tableName: 'users',
+      //         // defaultScope: {
+      //         //     where: {
+      //         //         status: 'Active'
+      //         //     }
+      //         // }
 
-    Users.associate = function (models) {
-        Users.hasMany(models.posts, {
-            as: 'posts',
-            foreignKey: 'userId',
-            sourceKey: 'uuid',
-        });
-    };
+      user_id: {
+        type: types.UUID,
+        defaultValue: types.UUIDV4,
+        primarykey: true,
+        unique: true,
+      },
+      first_name: {
+        type: types.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: types.STRING,
+        defaultValue: "",
+      },
+      user_name: {
+        type: types.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: types.STRING,
+        allowNull: false,
+      },
+      phone_no: {
+        type: types.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    },
+    {
+      tableName: "users",
+    }
+  );
 
-    return Users;
+  //   Users.associate = function (models) {
+  //     Users.hasMany(models.posts, {
+  //       as: "",
+  //       foreignKey: "userId",
+  //       sourceKey: "user_id",
+  //     });
+  //   };
+
+  return Users;
 };
