f7a51a13e2434e691edf794d58a4265b752e7f84
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-11-29 12:30:29 +0530

    router added

--- index.js
+++ index.js
@@ -16,77 +16,83 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(jsonParser);
 app.use(urlencodedParser);
 
-app.post("/save-user", async function (req, res) {
-  const usersCreate = await models.users.create({
-    name: req.body.name,
-  });
+const itemRouter = require("./routes/items.routes");
+app.use("/", itemRouter);
 
-  res.json({
-    usersCreate,
-  });
-});
+// app.post("/add-items", async function (req, res) {
+//   const itemsCreate = await models.items.create({
+//     item_name: req.body.item_name,
+//     item_content: req.body.item_content,
+//     price: req.body.price,
+//     item_count: req.body.item_count,
+//   });
 
-app.patch("/update-user", async function (req, res) {
-  const usersUpdate = await models.users.update(
-    {
-      name: req.body.name,
-    },
-    {
-      where: {
-        uuid: req.body.userid,
-      },
-      returning: true,
-    }
-  );
+//   res.json({
+//     itemsCreate,
+//   });
+// });
 
-  res.json({
-    usersUpdate,
-  });
-});
+// app.patch("/update-user", async function (req, res) {
+//   const usersUpdate = await models.users.update(
+//     {
+//       name: req.body.name,
+//     },
+//     {
+//       where: {
+//         uuid: req.body.userid,
+//       },
+//       returning: true,
+//     }
+//   );
 
-app.get("/", async function (req, res) {
-  try {
-    const usersFind = await models.users.findAndCountAll({
-      attributes: ["name"],
-      where: {
-        name: {
-          [Op.iLike]: `%${req.query.name}`,
-        },
-      },
-      logging: true,
-      include: [
-        {
-          model: models.posts,
-          as: "posts",
-          required: false,
-          where: {
-            content: {
-              [Op.not]: null,
-            },
-          },
-        },
-      ],
-    });
-    return res.json({
-      usersFind,
-    });
-  } catch (error) {
-    console.log("\n error...", error);
-    return res.send(error);
-  }
-});
+//   res.json({
+//     usersUpdate,
+//   });
+// });
 
-app.delete("/remove", async function (req, res) {
-  const pgRes = await pgClient.query(
-    "DELETE from users where userid=$1 RETURNING userid",
-    [req.query.userid]
-  );
+// app.get("/", async function (req, res) {
+//   try {
+//     const usersFind = await models.users.findAndCountAll({
+//       attributes: ["name"],
+//       where: {
+//         name: {
+//           [Op.iLike]: `%${req.query.name}`,
+//         },
+//       },
+//       logging: true,
+//       include: [
+//         {
+//           model: models.posts,
+//           as: "posts",
+//           required: false,
+//           where: {
+//             content: {
+//               [Op.not]: null,
+//             },
+//           },
+//         },
+//       ],
+//     });
+//     return res.json({
+//       usersFind,
+//     });
+//   } catch (error) {
+//     console.log("\n error...", error);
+//     return res.send(error);
+//   }
+// });
 
-  res.json({
-    rows: pgRes.rows,
-    count: pgRes.rowCount,
-  });
-});
+// app.delete("/remove", async function (req, res) {
+//   const pgRes = await pgClient.query(
+//     "DELETE from users where userid=$1 RETURNING userid",
+//     [req.query.userid]
+//   );
+
+//   res.json({
+//     rows: pgRes.rows,
+//     count: pgRes.rowCount,
+//   });
+// });
 
 app.listen(config.port, config.host, () => {
   console.log(`Server running at http://${config.host}:${config.port}/`);
