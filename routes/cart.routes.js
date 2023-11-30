8ac585d651e701dddf257cc9d482c4575ccb5227
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-11-30 11:44:39 +0530

    validate Cart

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { validate } = require("../middlewares/validate.middleware");
+const { addToCartSchema } = require("../validations/addToCart.schema");
 const addCartcontroller = require("../controllers/cart.controller");
-router.post("/add-cart", addCartcontroller);
+router.post("/add-cart", validate(addToCartSchema), addCartcontroller);
 module.exports = router;
--- validations/addToCart.schema.js
+++ validations/addToCart.schema.js
@@ -1,9 +1,7 @@
 const Joi = require("joi");
 const addToCartSchema = Joi.object({
-  item_id: Joi.number().required(),
-  item_name: Joi.string().required(),
-  user_id: Joi.number().required(),
-  item_price: Joi.number().precision(2).required(),
+  item_id: Joi.string().required(),
+  user_id: Joi.string().required(),
 });
 module.exports = {
   addToCartSchema,
