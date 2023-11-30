7b97ba4b1fd5c8f1aafb81c36aa29e7cb06af417
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-11-30 10:20:35 +0530

    folder rearrange

--- app/sql_schemas/favourites_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE favourites (
-    favourite_id SERIAL PRIMARY KEY,
-    item_id INT,
-    user_id INT,
-    CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES "users"(user_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
