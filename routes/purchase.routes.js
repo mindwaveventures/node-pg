ddc3fd16f8380053f8250ff305f2789ff00df826
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-11-30 11:37:14 +0530

    validate ratingValue

--- routes/purchase.routes.js
+++ routes/purchase.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const purchasesController = require("../controllers/purchases.controller");
-const listController = require("../controllers/purchases.controller");
+const { purchasesController } = require("../controllers/purchases.controller");
+const { listController } = require("../controllers/purchases.controller");
 router.post("/add-Purchases", purchasesController);
 router.get("/listBoughtItems/:user_id", listController);
 module.exports = router;
