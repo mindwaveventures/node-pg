77c3bb9e86414b57560209b00b5aec3a22606c5c
Vishnuprasanth-mediwave <vishnu_prasanth@mindwaveventures.com>
2023-11-30 19:03:24 +0530

    try catch added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,58 +1,76 @@
 const { models, Sequelize } = require("../config/sequelize-config");
 
 const addUserController = async (req, res, next) => {
-  const searchUser = await models.users.findAll({
-    attributes: ["email", "user_name"],
-    where: { email: req.body.email, user_name: req.body.user_name },
-  });
-  if (searchUser.length == 0) {
-    const usersCreate = await models.users.create({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      user_name: req.body.user_name,
-      user_password: req.body.user_password,
-      phone_no: req.body.phone_no,
+  try {
+    const searchUser = await models.users.findAll({
+      attributes: ["email", "user_name"],
+      where: { email: req.body.email, user_name: req.body.user_name },
     });
-    res.json({
-      usersCreate,
-    });
-  } else {
-    return next({
-      status: 400,
-      message: "user already exits, check the email and username",
+    if (searchUser.length == 0) {
+      const usersCreate = await models.users.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        user_name: req.body.user_name,
+        user_password: req.body.user_password,
+        phone_no: req.body.phone_no,
+      });
+      res.json({
+        usersCreate,
+      });
+    } else {
+      return next({
+        status: 400,
+        message: "user already exits, check the email and username",
+      });
+    }
+  } catch (error) {
+    return res.send({
+      message: error.errors.map((d) => d.message),
     });
   }
 };
 
 const loginController = async (req, res, next) => {
-  const searchUser = await models.users.findOne({
-    where: { email: req.body.email, user_password: req.body.user_password },
-  });
-  if (searchUser === null) {
-    return next({
-      status: 400,
-      message: "invalid email and username",
+  try {
+    const searchUser = await models.users.findOne({
+      where: { email: req.body.email, user_password: req.body.user_password },
     });
-  } else {
-    res.json({
-      searchUser,
+    if (searchUser === null) {
+      return next({
+        status: 400,
+        message: "invalid email and username",
+      });
+    } else {
+      res.json({
+        searchUser,
+      });
+    }
+  } catch (error) {
+    return res.send({
+      message: error.errors.map((d) => d.message),
     });
   }
 };
 
 const accountViewController = async (req, res, next) => {
-  const searchUser = await models.users.findOne({
-    where: { id: req.params.id },
-  });
-  if (searchUser === null) {
-    return next({
-      status: 400,
-      message: "user not found",
+  try {
+    const searchUser = await models.users.findOne({
+      where: { id: req.params.id },
     });
-  } else {
-    res.json({
-      searchUser,
+    if (searchUser === null) {
+      return next({
+        status: 400,
+        message: "user not found",
+      });
+    } else {
+      res.json({
+        searchUser,
+      });
+    }
+  } catch (error) {
+    return res.send({
+      message: error.errors.map((d) => d.message),
     });
   }
 };
